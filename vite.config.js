85c9d557622a27c40fc31002a3e6c4b510513f6c
omar <omarsrour181@gmail.com>
2025-02-24 14:24:29 +0200

    Updated vite.config.js and fixed build issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // تأكد أن `base` مضبوط
   server: {
-    hmr: true, // تأكد من تمكين HMR
+    hmr: true, // تمكين HMR
   },
 });
